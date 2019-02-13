c9656f1ce4ea1c915bb33795ba231ce6f58e299b
Abraham Hamidi <abraham.hamidi01@gmail.com>
2019-02-13 08:30:40 -0800

    Add file saving, display, and deletion

--- scripts/file-selection/FileButton.gd
+++ scripts/file-selection/FileButton.gd
@@ -1,3 +1,29 @@
 extends TextureButton
 
 export var id = 0;
+
+var _name = null;
+var _path = "";
+
+func _ready():
+    if !_name:
+        set_name_empty();
+    else:
+        set_name(_name);
+
+func set_name(var file_name):
+    get_node("RichTextLabel").bbcode_text = "[center]" + file_name + "[/center]";
+    _name = file_name;
+
+func set_name_empty():
+    set_name("Empty");
+    _name = null;
+
+func get_name():
+    return _name;
+
+func set_path(var path):
+    _path = path;
+
+func get_path():
+    return _path;
--- scripts/name-entry/NameEntry.gd
+++ scripts/name-entry/NameEntry.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export(NodePath) var emitter_path;
+onready var file_id;
 
 func _ready():
     var emitter_node = get_node(emitter_path);
@@ -8,7 +9,14 @@ func _ready():
     emitter_node.connect("cancel", self, "_cancel");
 
 func _submit(name):
-    print(name);
+    var dir = Directory.new();
+    if (!dir.dir_exists("user://save")):
+        dir.make_dir_recursive("user://save");
+    var new_file = File.new();
+    new_file.open("user://save/file" + str(file_id + 1) + ".sav", File.WRITE);
+    new_file.store_8(file_id);
+    new_file.store_pascal_string(name);
+    new_file.close();
     get_tree().change_scene(Statics.file_selection_scene_path);
 
 func _cancel():
