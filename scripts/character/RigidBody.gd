9d3b731b69b546581039ff7960cd8e77de66a903
Boyd <BOYDWIL000@mysbisd.org>
2019-03-04 09:07:21 -0600

    Add level 1 rev0

---
+++ scripts/character/RigidBody.gd
@@ -0,0 +1,21 @@
+extends RigidBody
+
+var speed = .3
+
+func _ready():
+	set_process(true)
+
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		#print("key left")
+		apply_impulse(Vector3(0,0,0),Vector3(-speed,0,0))
+	if Input.is_key_pressed(KEY_RIGHT):
+		#print("key right")
+		apply_impulse(Vector3(0,0,0),Vector3(speed,0,0))
+	if Input.is_key_pressed(KEY_UP):
+		#print("key up")
+		apply_impulse(Vector3(0,0,0),Vector3(0,0,-speed))
+	if Input.is_key_pressed(KEY_DOWN):
+		#print("key down")
+		apply_impulse(Vector3(0,0,0),Vector3(0,0,speed))
