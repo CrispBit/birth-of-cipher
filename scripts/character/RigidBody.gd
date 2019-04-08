d29389e938c3eebe4e293a2dfd45c06464f02d63
Abraham Hamidi <abraham.hamidi01@gmail.com>
2019-04-08 06:48:02 -0700

    Fix formatting in RigidBody script

--- scripts/character/RigidBody.gd
+++ scripts/character/RigidBody.gd
@@ -1,21 +1,21 @@
 extends RigidBody
 
-var speed = .3
+var speed = .1
 
 func _ready():
-	set_process(true)
+    set_process(true)
 
 
 func _process(delta):
-	if Input.is_key_pressed(KEY_LEFT):
-		#print("key left")
-		apply_impulse(Vector3(0,0,0),Vector3(-speed,0,0))
-	if Input.is_key_pressed(KEY_RIGHT):
-		#print("key right")
-		apply_impulse(Vector3(0,0,0),Vector3(speed,0,0))
-	if Input.is_key_pressed(KEY_UP):
-		#print("key up")
-		apply_impulse(Vector3(0,0,0),Vector3(0,0,-speed))
-	if Input.is_key_pressed(KEY_DOWN):
-		#print("key down")
-		apply_impulse(Vector3(0,0,0),Vector3(0,0,speed))
+    if Input.is_key_pressed(KEY_LEFT):
+        #print("key left")
+        apply_impulse(Vector3(0,0,0),Vector3(-speed,0,0))
+    if Input.is_key_pressed(KEY_RIGHT):
+        #print("key right")
+        apply_impulse(Vector3(0,0,0),Vector3(speed,0,0))
+    if Input.is_key_pressed(KEY_UP):
+        #print("key up")
+        apply_impulse(Vector3(0,0,0),Vector3(0,0,-speed))
+    if Input.is_key_pressed(KEY_DOWN):
+        #print("key down")
+        apply_impulse(Vector3(0,0,0),Vector3(0,0,speed))
