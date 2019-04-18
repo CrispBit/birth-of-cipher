5e59226600f07e47ec217184cb685622ac663760
Abraham Hamidi <abraham.hamidi01@gmail.com>
2019-04-18 06:51:13 -0700

    Lock hacker rotation to y axis

--- scripts/level-controller/level_controller.gd
+++ scripts/level-controller/level_controller.gd
@@ -2,12 +2,14 @@ extends Container
 
 var _data;
 enum CONTROLLER {
-    mouse_keyboard,
-    joypad,
+    MOUSE_KEYBOARD,
+    JOYPAD,
 }
-var controller = CONTROLLER.mouse_keyboard;
+var controller = CONTROLLER.MOUSE_KEYBOARD;
 var _player_entity;
+var _last_dialogue_entity;
 var _last_interactable_entity;
+var _last_interactable_entity_interact_type;
 var _interactables;
 
 func set_data(data):
@@ -29,26 +31,37 @@ func _load_level(region_id, area_id):
 
 func _input(event):
     if event is InputEventKey or event is InputEventMouse:
-        controller = CONTROLLER.mouse_keyboard;
+        controller = CONTROLLER.MOUSE_KEYBOARD;
     elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
-        controller = CONTROLLER.joypad;
+        controller = CONTROLLER.JOYPAD;
     else: return;
-    $level_container/controller_icons/keyboard_mouse.visible = false;
-    $level_container/controller_icons/joypad.visible = false;
+    $controller_icons/keyboard_mouse.visible = false;
+    $controller_icons/joypad.visible = false;
     match controller:
-        CONTROLLER.mouse_keyboard:
-            $level_container/controller_icons/keyboard_mouse.visible = true;
-        CONTROLLER.joypad:
-            $level_container/controller_icons/joypad.visible = true;
+        CONTROLLER.MOUSE_KEYBOARD:
+            $controller_icons/keyboard_mouse.visible = true;
+        CONTROLLER.JOYPAD:
+            $controller_icons/joypad.visible = true;
     if Input.is_action_just_pressed("game_interact"):
-        _last_interactable_entity.interact();
+        if _last_interactable_entity:
+            match _last_interactable_entity_interact_type:
+                Statics.INTERACT_TYPE.DIALOGUE:
+                    if _last_interactable_entity != _last_dialogue_entity:
+                        $DialogueController.init_dialogue(_last_interactable_entity.get_node("DialogueComponent"));
+                        _last_dialogue_entity = _last_interactable_entity;
+                    $DialogueController.step_dialogue();
+                Statics.INTERACT_TYPE.NONE:
+                    pass
 
 func _process(delta):
-    for item in $level_container/controller_hint_icons/keyboard.get_children():
+    _last_interactable_entity = null;
+    for item in $controller_hint_icons/keyboard.get_children():
         item.visible = false;
     for interactable in _interactables:
-        if interactable.can_interact_with(_player_entity):
+        var interact_type = interactable.get_interact_type(_player_entity);
+        if interact_type != Statics.INTERACT_TYPE.NONE:
             _last_interactable_entity = interactable;
+            _last_interactable_entity_interact_type = interact_type;
             for action in InputMap.get_action_list("game_interact"):
-                if controller == CONTROLLER.mouse_keyboard && action is InputEventKey:
-                    get_node("level_container/controller_hint_icons/keyboard/" + OS.get_scancode_string(action.scancode)).visible = true;
+                if controller == CONTROLLER.MOUSE_KEYBOARD && action is InputEventKey:
+                    get_node("controller_hint_icons/keyboard/" + OS.get_scancode_string(action.scancode)).visible = true;
