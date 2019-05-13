998486e414ab1216b4ca78720faa3cb4cdf7955e
Abraham Hamidi <abraham.hamidi01@gmail.com>
2019-05-13 06:44:04 -0700

    Add remy as model for two

--- scripts/levels/region-1/area_1.gd
+++ scripts/levels/region-1/area_1.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
 func _process(delta):
-    for hacker in get_tree().get_nodes_in_group("interactable"):
-        hacker.get_node("RigidBody").look_at(2 * hacker.get_node("RigidBody").global_transform.origin - $Player.get_node("KinematicBody").global_transform.origin, Vector3(0, 1, 0));
-        hacker.get_node("RigidBody").set_rotation(Vector3(0, hacker.get_node("RigidBody").get_rotation().y, 0));
+    for person in get_tree().get_nodes_in_group("interactable"):
+        person.get_node("RigidBody").look_at(2 * person.get_node("RigidBody").global_transform.origin - $Player.get_node("KinematicBody").global_transform.origin, Vector3(0, 1, 0));
+        person.get_node("RigidBody").set_rotation(Vector3(0, person.get_node("RigidBody").get_rotation().y, 0));
